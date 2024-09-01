a3d3dd4d167f8379856feeac2856ed987c82eb52
mshradtya <mshradtya@gmail.com>
2024-09-01 18:18:43 +0530

    create file function done

--- node/file-system/project.js
+++ node/file-system/project.js
@@ -1,6 +1,24 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const createFile = async (path) => {
+    try {
+      // checking if file already exists
+      const existingFileHandle = await fs.open(path, "r");
+      existingFileHandle.close();
+      // file exists
+      return console.log(`The filr ${path} already exists`);
+    } catch (err) {
+      // we don't have the file, so we can create it
+      const newFileHandle = await fs.open(path, "w");
+      console.log("a new file was successfully created");
+      newFileHandle.close();
+    }
+  };
+
+  // commands
+  const CREATE_FILE = "create a file";
+
   const commandFileHandler = await fs.open("./command.txt", "r"); // open the file
 
   commandFileHandler.on("change", async () => {
@@ -18,7 +36,14 @@ const fs = require("fs/promises");
     // we always want to read the whole content ( from beginning all the way to the end )
     await commandFileHandler.read(buff, offset, length, position);
 
-    console.log(buff.toString("utf-8"));
+    const command = buff.toString("utf-8");
+
+    // create a file:
+    // create a file <path>
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command.substring(CREATE_FILE.length + 1);
+      createFile(filePath);
+    }
   });
 
   // watcher
