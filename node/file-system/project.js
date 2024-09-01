eb617f2ccc8ad2a8218e6a872dc45a2f78bf491e
mshradtya <mshradtya@gmail.com>
2024-09-01 12:46:18 +0530

    opening a file and reading its content

--- node/file-system/project.js
+++ node/file-system/project.js
@@ -1,6 +1,8 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const commandFileHandler = await fs.open("./command.txt", "r"); // open the file
+
   const watcher = fs.watch("./command.txt");
 
   // async iterator
@@ -8,6 +10,22 @@ const fs = require("fs/promises");
     if (event.eventType === "change") {
       // the file was changed
       console.log("The File was changed");
+      // we want to read the content
+      // get the size of our file
+      const size = (await commandFileHandler.stat()).size;
+      const buff = Buffer.alloc(size);
+
+      const offset = 0;
+      const length = buff.byteLength;
+      const position = 0;
+
+      const content = await commandFileHandler.read(
+        buff,
+        offset,
+        length,
+        position
+      );
+      console.log(content);
     }
   }
 })();
