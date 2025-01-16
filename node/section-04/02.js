aae6cafb7a56becdb26a0b0df84c723c874aae8b
Aditya Mishra <mshradtya@gmail.com>
2025-01-16 22:13:38 +0530

    re-wrote code myself

--- node/section-04/02.js
+++ node/section-04/02.js
@@ -4,12 +4,14 @@ const fs = require("node:fs");
 // Time taken to execute: 262.334ms
 // Memory Usage: 10MB
 
-(async () => {
-  console.time("writeMany");
-  fs.open("test.txt", "w", (err, fd) => {
+(() => {
+  console.time("writing");
+  fs.open("./test.txt", "w", (err, fd) => {
     for (let i = 0; i < 100000; i++) {
       fs.writeSync(fd, ` ${i} `);
     }
-    console.timeEnd("writeMany");
+    fs.close(fd);
   });
+
+  console.timeEnd("writing");
 })();
--- node/test.js
+++ node/test.js
@@ -1,16 +1,9 @@
-// fixing the the loop by properly using streams
-
+// writing into a file a million times
 const fs = require("fs/promises");
-
-// Time taken to execute: 234.443ms
-// Memory Usage: 18MB
-
-// time and memory usage, both have reduced significantly
 
 (async () => {
   console.time("written");
   const fileHandle = await fs.open("./test.txt", "w");
-  // creating a write stream of this file
   const stream = fileHandle.createWriteStream();
 
   let i = 0;
@@ -18,16 +11,14 @@ const fs = require("fs/promises");
 
   const write = () => {
     for (i; i < LIMIT; i++) {
-      if (!stream.write(` ${i} `)) break; // if stream.write() returns false, stop the loop
+      if (!stream.write(` ${i} `)) break;
     }
-    // this is our last write
     if (i === LIMIT) {
       stream.end();
       console.timeEnd("written");
     }
   };
 
-  // resume loop when stream's internal buffer is empty
   stream.on("drain", () => {
     write();
   });
